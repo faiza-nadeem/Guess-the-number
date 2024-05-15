2624dea8cb5cb19e6c89f1e1bce624f2808f61ab
faiza <168096871+faiza-nadeem@users.noreply.github.com>
2024-05-15 14:25:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+ 
+ 
+ 
+ var uname=prompt("Guess my Birth Month?")
+     if(
+      uname==="10"
+     ) {
+      alert("well done you are right ('_')")
+     }                                      
+     else if (uname==="11") 
+      {alert("you are close:try again")}    
+     else if (uname==="1") 
+        {alert("you are  too far :try again")} 
+     else{
+      alert("try again")
+     }
